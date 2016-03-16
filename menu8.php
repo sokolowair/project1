6f0e5714ddf11862f772154b933e7a23192b02ec
sokolowair <sokolow1507@gmail.com>
2016-03-17 00:36:06 +0200

    Update menu8.php

--- menu8.php
+++ menu8.php
@@ -27,8 +27,18 @@ img.revo {
   border-radius:20px;
 }
 </style>
+<link href="style.css" rel="stylesheet" >
+<link href="style2.css" rel="stylesheet" >
 </head>
 <body>
+
+<?php 
+
+ $tmp=file_get_contents("menu.html");
+ echo $tmp;
+   
+
+?>
 <img class=revo src="http://energyandcarbon.com/wp-content/uploads/2015/01/images.duckduckgo.com_1.jpg">
 
 <?php require 'hello.php';?>
@@ -42,19 +52,25 @@ Revised on 20 Nov 2015. <br>
 What is required is for the political will to be there. <br>
 <p>Greenpeace has been publishing its Energy [R]evolution scenarios since 2005, more recently in collaboration with the scientific community, in particular the German Aerospace Centre (DLr). <br>While our predictions on the potential and market growth of renewable energy may once have seemed fanciful or unrealistic, they have proved to be accurate. the US-based Meister Consultants Group concluded earlier this year that "the world's biggest energy agencies, financial institutions and fossil fuel companies for the most part seriously under-estimated just how fast the clean power sector could and would grow".<br> It wasn't the IEA, Goldman Sachs or the US Department of Energy who got it right. It was Greenpeace's market scenario which was the most accurate. <br>
 REVISIONS:
-15 October 2015 – The Full version of this report has been revised to include a Foreward.
+15 October 2015 â€“ The Full version of this report has been revised to include a Foreward.
 
-30 September 2015 – The Sumary version of this report has been revised to edit an employment figure on Page 10. 
+30 September 2015 â€“ The Sumary version of this report has been revised to edit an employment figure on Page 10. 
 
-20 November 2015 – Greenpeace received an overwhelming amount of positive reaction to the new and first global 100% Renewable Energy pathway. Scientist, energy experts and industry representatives sent their support and critique over the past two months. <br> We are very pleased to have the support of Adnan Z. Amin, Director-General of the International Renewable Energy Agency (IRENA) who wrote a foreword for the new Energy [R]evolution 2015. Based on all these valuable comments, two corrections in our over 350 page documentation have been made in the new pdf: <br>
+20 November 2015 â€“ Greenpeace received an overwhelming amount of positive reaction to the new and first global 100% Renewable Energy pathway. Scientist, energy experts and industry representatives sent their support and critique over the past two months. <br> We are very pleased to have the support of Adnan Z. Amin, Director-General of the International Renewable Energy Agency (IRENA) who wrote a foreword for the new Energy [R]evolution 2015. Based on all these valuable comments, two corrections in our over 350 page documentation have been made in the new pdf: <br>
 
 Updated employment figures:
-<p>While the overall positive effect of the Energy [R]evolution scenario by 2030 – compared to 2015 – remains 19 million additional jobs, we corrected an administrative error which led to the inclusion of draft results and not the final data set. The projection of total energy sector employment for 2030 should have included 46 million jobs in the Energy [R]evolution scenario, and 27 million in the Reference scenario, rather than 48 million and 28 million. <br>
+<p>While the overall positive effect of the Energy [R]evolution scenario by 2030 â€“ compared to 2015 â€“ remains 19 million additional jobs, we corrected an administrative error which led to the inclusion of draft results and not the final data set. The projection of total energy sector employment for 2030 should have included 46 million jobs in the Energy [R]evolution scenario, and 27 million in the Reference scenario, rather than 48 million and 28 million. <br>
 
 <p>Corrected Renewable Heating figure for hydrogen based heating generation
 While the calculation of the heating sector for the 10 world regions has been correct, there was a mistake in the global reporting sheet, which showed the hydrogen share incorrectly. While the overall deviation was significantly below 5%, we decided to correct these figures throughout the report in tables and figures in order to avoid further confusion about the rate of decarbonisation achieved in the scenarios. It is important to note that overall results remain the same. <br>
 
 <p>Greenpeace would like to thank all experts for their critical feedback which helps to improve the scientific value even further. We are committed to develop and publish only high quality energy scenarios and correct every inaccuracy immediately. Thank you for your ongoing support. </h3><br>
 
+
+<?php
+$tmp=file_get_contents("footer.html");
+echo $tmp;
+
+?>
 </body>
 </html>
