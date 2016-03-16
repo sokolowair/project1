e9e3d895ce40707e1d92f7d77183cd15ea73be0b
sokolowair <sokolow1507@gmail.com>
2016-03-17 00:36:44 +0200

    Update menu9.php

--- menu9.php
+++ menu9.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Clicking Clean</title>
+<title>Alternative Energy Sources</title>
 <style>
   body {
    background-color: light-grey ;
@@ -11,7 +11,6 @@
   h1 {
 
    text-align: left;
-   color:#004d00;
 
 }
   h3 {
@@ -19,29 +18,59 @@
   text-align: left;
 
 }
-img.click {
+img.revo {
    
   float:left;
   margin-left: 100px;
-  width:800px;
+  width:500px;
   height: 300px;
   border-radius:20px;
 }
 </style>
+<link href="style.css" rel="stylesheet" >
+<link href="style2.css" rel="stylesheet" >
 </head>
 <body>
-<img class="click" src="http://www.pressenza.com/wp-content/uploads/2015/05/ClickCleanOpenSpace-720x270.jpg">
+
+<?php 
+
+ $tmp=file_get_contents("menu.html");
+ echo $tmp;
+   
+
+?>
+<img class=revo src="http://energyandcarbon.com/wp-content/uploads/2015/01/images.duckduckgo.com_1.jpg">
+
 <?php require 'hello.php';?>
 
 
 
-<h1>Clicking Clean</h1>
-A Guide to Building the Green Internet
-Publication - 12 May, 2015
-<h3><p>While there may be significant energy efficiency gains from moving our lives online, the explosive growth of our digital lives is outstripping those gains. Publishing conglomerates now consume more energy from their data centers than their printing presses. Greenpeace has estimated that the aggregate electricity demand of our digital infrastructure back in 2011 would have ranked sixth in the world among countries.<br>
-<p>The rapid transition to streaming video models, as well as tablets and other thin client devices that supplant on-device storage with the cloud, means more and more demand for data center capacity, which will require more energy to power.<br>
-<p>The transition to online distribution models, such as video streaming, appears to deliver a reduction in the carbon footprint over traditional models of delivery. However, in some cases, this shift may simply be enabling much higher levels of consumption, ultimately increasing the total amount of electricity consumed and the associated pollution from electricity generation.<br> Unless leading internet companies find a way to leapfrog traditional, polluting sources of electricity, the convenience of streaming could cause us to increase our carbon footprint.<br></h3>
+<h1>Energy Revolution 2015<h1><br>
+<h3>This is the year when the fight against climate change could take a dramatic turn. The conference in Paris in December presents political and business leaders with the opportunity to take the critical decisions needed if we are to keep average temperature rises to no more than 1.5 or 2 degrees Celsius. According to the IPCC, humankind cannot emit more than 1,000 giga-tonnes of CO2 from now, if we are to stay within this limit. At the current and projected rate of consumption, this entire carbon budget will be used by 2040. <br>
+Revised on 20 Nov 2015. <br>
+<p>Dynamic change is happening in energy supply, but the change needs to happen faster. this Energy [R]evolution scenario proposes a pathway to a 100% sustainable energy supply, ending CO2 emissions and phasing out nuclear energy, and making redundant new oil exploration in the arctic and deep sea waters such as off the coast of Brazil. It also demonstrates that this transformation increases employment in the energy sector. <br>
+What is required is for the political will to be there. <br>
+<p>Greenpeace has been publishing its Energy [R]evolution scenarios since 2005, more recently in collaboration with the scientific community, in particular the German Aerospace Centre (DLr). <br>While our predictions on the potential and market growth of renewable energy may once have seemed fanciful or unrealistic, they have proved to be accurate. the US-based Meister Consultants Group concluded earlier this year that "the world's biggest energy agencies, financial institutions and fossil fuel companies for the most part seriously under-estimated just how fast the clean power sector could and would grow".<br> It wasn't the IEA, Goldman Sachs or the US Department of Energy who got it right. It was Greenpeace's market scenario which was the most accurate. <br>
+REVISIONS:
+15 October 2015 – The Full version of this report has been revised to include a Foreward.
+
+30 September 2015 – The Sumary version of this report has been revised to edit an employment figure on Page 10. 
+
+20 November 2015 – Greenpeace received an overwhelming amount of positive reaction to the new and first global 100% Renewable Energy pathway. Scientist, energy experts and industry representatives sent their support and critique over the past two months. <br> We are very pleased to have the support of Adnan Z. Amin, Director-General of the International Renewable Energy Agency (IRENA) who wrote a foreword for the new Energy [R]evolution 2015. Based on all these valuable comments, two corrections in our over 350 page documentation have been made in the new pdf: <br>
+
+Updated employment figures:
+<p>While the overall positive effect of the Energy [R]evolution scenario by 2030 – compared to 2015 – remains 19 million additional jobs, we corrected an administrative error which led to the inclusion of draft results and not the final data set. The projection of total energy sector employment for 2030 should have included 46 million jobs in the Energy [R]evolution scenario, and 27 million in the Reference scenario, rather than 48 million and 28 million. <br>
+
+<p>Corrected Renewable Heating figure for hydrogen based heating generation
+While the calculation of the heating sector for the 10 world regions has been correct, there was a mistake in the global reporting sheet, which showed the hydrogen share incorrectly. While the overall deviation was significantly below 5%, we decided to correct these figures throughout the report in tables and figures in order to avoid further confusion about the rate of decarbonisation achieved in the scenarios. It is important to note that overall results remain the same. <br>
+
+<p>Greenpeace would like to thank all experts for their critical feedback which helps to improve the scientific value even further. We are committed to develop and publish only high quality energy scenarios and correct every inaccuracy immediately. Thank you for your ongoing support. </h3><br>
 
 
+<?php
+$tmp=file_get_contents("footer.html");
+echo $tmp;
+
+?>
 </body>
 </html>
